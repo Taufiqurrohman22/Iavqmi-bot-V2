f0d075e736203d453c50337b66b16cbbbec7f1d8
Taufiqurrohman22 <86914986+Taufiqurrohman22@users.noreply.github.com>
2022-11-01 10:57:56 +0700

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -4,19 +4,19 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
 let krtu = `☰⃟⟣⟔ Yᴏᴜʀ Cᴀʀᴅ Iɴᴛʀᴏ ⟓⟢⃟
-┇⃟⟣⟜ *Nama:* 
-┇⃟⟣⟜ *Umur:* 
-┇⃟⟣⟜ *Alamat:*
-┇⃟⟣⟜ *Hobi:*
-┇⃟⟣⟜ *Pasangan:*
+┇⃟⟣⟜ *Nama:Taufiqurrohman* 
+┇⃟⟣⟜ *Umur:16* 
+┇⃟⟣⟜ *Alamat:Desa Heuleut*
+┇⃟⟣⟜ *Hobi:Coding*
+┇⃟⟣⟜ *Pasangan:¿*
 ┖┈┈┈╼╸╸╸╴╴╴ ╳
 `
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, krtu, wm,'MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://facebook.com/sadtime098",
+    mediaUrl: "https://www.facebook.com/taufiqurrohman.opik.750",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
+    description: "https://www.instagram.com/reel/CgFN7p4tuI9/?igshid=MDJmNzVkMjY=", 
     title: 'Iɴᴛʀᴏᴄᴀᴅᴜᴛɪᴏɴ',
     body: wm,
     thumbnail: thumb,
